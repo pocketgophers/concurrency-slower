1a94e46db706799a406307acca88c3d628923014
Nathan Kerr <nathan@nathankerr.com>
2017-09-05 23:12:07 +0200

    extract function

--- main.go
+++ main.go
@@ -7,13 +7,25 @@ import (
 	"time"
 )
 
-var list = [][]int{}
+const (
+	numberToGenerate = 1000000
+)
 
 func main() {
+	start := time.Now()
+	fmt.Println("Start")
 
+	list := lottoNumbers(numberToGenerate)
+
+	d := time.Now().Sub(start)
+	fmt.Println("End ", len(list), " ", d.Seconds())
+}
+
+func lottoNumbers(n int) [][]int {
+	var list = [][]int{}
 	rand.Seed(time.Now().UnixNano())
 
-	rec := make(chan []int, 10000000)
+	rec := make(chan []int, n)
 
 	go func(c chan []int) {
 		for {
@@ -21,21 +33,19 @@ func main() {
 		}
 	}(rec)
 
-	start := time.Now()
-	fmt.Println("Start")
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		func() {
 			defer wg.Done()
-			for i := 0; i < 1000000; i++ {
+			for i := 0; i < n; i++ {
 				add(rec)
 			}
 		}()
 	}
 	wg.Wait()
-	d := time.Now().Sub(start)
-	fmt.Println("End ", len(list), " ", d.Seconds())
+
+	return list
 }
 
 func add(c chan []int) {
